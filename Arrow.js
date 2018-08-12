0896b8f49591fd9fbde48c67d369cf82c8226a31
test <zino1187@gmail.com>
2018-08-12 15:54:24 +0900

    no message

--- Arrow.js
+++ Arrow.js
@@ -10,19 +10,21 @@ class Arrow{
 	//밴더사에서 이미 정해놓은 함수이다!! 따라서 오타나지 말자..
 	//생성자 함수는 객체가 태어날때 강제 호출되므로, 개발자는 개성 있는 물건을
 	//생성하려면 이 기회를 놓치지 말아야 한다..
-	constructor(left, top, velX){
+	constructor(left, top, velX,color){
 		this.span;
 		this.x=0; 
 		this.velX=velX; //화살의 x 축방향의 속도를 결정하는 변수
 		//화살의 위치 지정용 변수~!!
 		this.left=left;
 		this.top=top;
+		this.color=color;
 	}
 
 	//화살 생성
 	init(){
 		this.span=document.createElement("span");
 		this.span.innerText="→";
+		this.span.style.color=this.color;
 		this.span.style.position="absolute";
 		this.span.style.left=this.left+"px";
 		this.span.style.top=this.top+"px";
